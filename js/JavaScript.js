df607336303f3bd6e9d86e649060d861185bcbf4
Wilfran <142626346+wpajaro@users.noreply.github.com>
2024-02-16 13:17:44 -0500

    Add files via upload

---
+++ js/JavaScript.js
@@ -0,0 +1,8 @@
+let nombre = "Wilfran";
+//alert (`Hola ${nombre}` );      //Hola Wilfran
+
+let arreglo = [nombre = "Wilfran", edad = 27, altura = 1.64];
+
+arreglo.unshift("Es el más perrón");
+
+//alert(arreglo[0]);
